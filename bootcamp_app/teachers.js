09de88fcdba9d0fe5201967949a1748839517d0f
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-15 05:00:05 +0000

    Connected to a PostgreSQL dtabase and added some queries into the students and teachers js file

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -12,15 +12,17 @@ const pool = new Pool({
 // when we make the query from a JS application, the results come back as a JS object. 
 //Once the .then(res => {} ) gets executed, we're not dealing with SQL or the database anymore, we're just dealing with JS objects
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id=cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON assistance_requests.teacher_id = teachers.id 
+JOIN students ON assistance_requests.student_id = students.id 
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teachers.name;
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
